d62400efaf48c19da31f1e6a100c3342ddf195f3
Marianatprograma <maritaborue@gmail.com>
2022-08-27 23:15:33 -0500

    cambio de origin del cors

--- index.js
+++ index.js
@@ -3,18 +3,24 @@ const cors = require('cors');
 require('dotenv').config();
 require("./db/database");
 
-
-const app = express();
-
 const subredditsRoutes = require("./routes/subreddit.js")
 
+const app = express();
+app.use(cors());
+
+// var corsOptions = {
+//     origin: 'http://localhost:8080',
+//     optionsSuccessStatus: 200, // For legacy browser support
+//     methods: "GET, PUT"
+// }
+
+// app.use(cors(corsOptions));
 
 app.use(subredditsRoutes)
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.use(cors())
 
 
 app.listen(process.env.PORT || 5000, () =>{
